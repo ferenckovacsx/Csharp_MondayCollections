221cec5a13decce37f56300dd613a95afd057a50
Ferenc Kovacs <ferenckovacsx@gmail.com>
2016-04-19 11:59:06 +0200

    add string organizer to mondaycollections

---
+++ StringOrganizer.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MondayCollections_OrganizeStrings
+{
+    class StringOrganizer
+    {
+        public static void Main()
+        {
+            ArrayList arrayOfStrings = new ArrayList();
+            string input = "";
+
+            Console.WriteLine("Write some text then press Enter. This text will be added to the list.");
+            input = Console.ReadLine();
+            arrayOfStrings.Add(input);
+
+            while (input.Length > 0)
+            {
+                Console.WriteLine("Add another string then press Enter. Submit an empty string when you are done!");
+                input = Console.ReadLine();
+                arrayOfStrings.Add(input);
+            }
+
+            Console.WriteLine("Below you can find the original list of strings (unsorted): ");
+
+            foreach (var item in arrayOfStrings)
+            {
+                Console.WriteLine(item);
+            }
+
+            arrayOfStrings.Sort();
+
+            Console.WriteLine("Here is the list of strings organized alphabetically: ");
+
+            foreach (var item in arrayOfStrings)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
